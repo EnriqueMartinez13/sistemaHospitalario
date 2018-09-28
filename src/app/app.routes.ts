16b058440144315711b51099588fb78a525d3efb
Luis Enriqeu <rpg_chino@hotmail.com>
2018-09-28 00:35:56 -0500

    Agregamos y modificamos modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 const appRoutes: Routes = [
-    {
+    /*{
         path: '',
         component: PagesComponent,
         children: [
@@ -20,11 +15,11 @@ const appRoutes: Routes = [
             { path: 'progress', component: ProgressComponent },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
-    },
+    },*/
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-//  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     { path: '**', component: NopagefoundComponent }
+    //{ path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true});
